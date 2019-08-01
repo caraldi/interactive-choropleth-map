436aaf89ac32a312be98e4c01f86329412d28d0f
carald <caitlin.araldi@nike.com>
2019-08-01 12:18:37 -0700

    feat: Add component for choropleth map

---
+++ src/components/USChoroplethMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="c-wrapper">
+    <svg
+      width="960"
+      height="600"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'USChoroplethMap'
+}
+</script>
+
+<style scoped lang="scss">
+.c-wrapper {
+  border: 1px solid #e0e0e0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Interactive Map with Category Filters"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'HomeView',
   components: {
     HelloWorld
   }
